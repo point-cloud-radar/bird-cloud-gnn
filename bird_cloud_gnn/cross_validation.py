154ec4e8265d506714d6effeb6a923f235eecb1b
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-06-20 11:53:15 +0200

    [WIP] Create model.fit and model.evaluate functions

---
+++ bird_cloud_gnn/cross_validation.py
@@ -0,0 +1,66 @@
+"""Helper functions for cross validation.
+"""
+import numpy as np
+from dgl.dataloading import GraphDataLoader
+from sklearn.model_selection import StratifiedKFold
+from torch.utils.data.sampler import SubsetRandomSampler
+from tqdm import tqdm
+from bird_cloud_gnn.gnn_model import GCN
+
+
+# pylint: disable=too-many-arguments, too-many-locals
+def kfold_evaluate(
+    dataset,
+    h_feats=16,
+    n_splits=5,
+    learning_rate=0.01,
+    num_epochs=100,
+    batch_size=512,
+):
+    """
+    Evaluate the model on a dataset using StratifiedKFold.
+
+    Args:
+        dataset (RadarDataset): The dataset
+        h_feats (int, optional): The number of hidden features of the model
+        n_splits (int, optional): Number of folds. Defaults to 5.
+        learning_rate (float, optional): Learning rate. Defaults to 0.01.
+        num_epochs (int, optional): Training epochs. Defaults to 20.
+        batch_size (int, optional): Batch size used in the data loaders. Defaults to 512.
+    """
+
+    labels = np.array(dataset.labels)
+    # Initialize a stratified k-fold splitter
+    kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+    progress_bar = tqdm(total=n_splits)
+    # Perform k-fold cross-validation
+    for fold, (train_idx, test_idx) in enumerate(kfold.split(dataset, labels)):
+        train_sampler = SubsetRandomSampler(train_idx)
+        test_sampler = SubsetRandomSampler(test_idx)
+
+        train_dataloader = GraphDataLoader(
+            dataset=dataset,
+            sampler=train_sampler,
+            batch_size=batch_size,
+            drop_last=False,
+        )
+        test_dataloader = GraphDataLoader(
+            dataset=dataset,
+            sampler=test_sampler,
+            batch_size=batch_size,
+            drop_last=False,
+        )
+
+        learning_rate = 0.01
+        model = GCN(
+            in_feats=len(dataset.features),
+            h_feats=h_feats,
+            num_classes=2,
+        )
+        model.fit(train_dataloader, learning_rate=learning_rate, num_epochs=num_epochs)
+
+        accuracy = model.evaluate(test_dataloader)
+        print(f"Fold {fold + 1} - Test accuracy: {accuracy}")
+
+        progress_bar.set_postfix({"Fold": fold + 1})
+        progress_bar.update(1)
--- bird_cloud_gnn/gnn_model.py
+++ bird_cloud_gnn/gnn_model.py
@@ -5,6 +5,7 @@ import dgl
 import torch.nn.functional as F
 from dgl.nn.pytorch.conv import GraphConv
 from torch import nn
+from torch import optim
 
 
 os.environ["DGLBACKEND"] = "pytorch"
@@ -56,3 +57,46 @@ class GCN(nn.Module):
         h = self.conv2(g, h)
         g.ndata["h"] = h
         return dgl.mean_nodes(g, "h")
+
+    def fit(self, train_dataloader, learning_rate=0.01, num_epochs=20):
+        """
+        Train the model.
+
+        Args:
+            train_dataloader: Data loader, such as `SubsetRandomSampler`
+            learning_rate (float, optional): Learning rate passed to the optimization. Defaults to 0.01.
+            num_epochs (int, optional): Number of epochs of training. Defaults to 20.
+        """
+        self.train()
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        for _ in range(num_epochs):
+            for batched_graph, labels in train_dataloader:
+                pred = self(batched_graph, batched_graph.ndata["x"].float())
+                loss = nn.functional.cross_entropy(pred, labels)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                assert pred.dim() == self.num_classes
+
+    def evaluate(self, test_dataloader):
+        """
+        Evaluate model.
+
+        Args:
+            test_dataloader: Data loader, such as `SubsetRandomSampler`.
+
+        Returns:
+            accuracy: Accuracy
+        """
+        self.eval()
+        num_correct = 0
+        num_tests = 0
+
+        for batched_graph, labels in test_dataloader:
+            pred = self(batched_graph, batched_graph.ndata["x"].float())
+            num_correct += (pred.argmax(1) == labels).sum().item()
+            num_tests += len(labels)
+            assert pred.dim() == self.num_classes
+
+        accuracy = num_correct / num_tests
+        return accuracy
