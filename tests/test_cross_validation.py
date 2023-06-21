8254bfcf6b8ee658b4f3768c5f51be1cba4f2906
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-06-21 09:53:01 +0200

    Add Leave one origin out cross validation

--- tests/test_cross_validation.py
+++ tests/test_cross_validation.py
@@ -1,11 +1,21 @@
 """Tests for cross_validation"""
 from bird_cloud_gnn.cross_validation import kfold_evaluate
+from bird_cloud_gnn.cross_validation import leave_one_origin_out_evaluate
 
 
 def test_kfold_evaluate(dataset_fixture):
     """Test cross validation with KFold"""
 
     kfold_evaluate(
+        dataset_fixture,
+        h_feats=32,
+    )
+
+
+def test_leave_one_out_evaluate(dataset_fixture):
+    """Test cross validation with leave-one-out"""
+
+    leave_one_origin_out_evaluate(
         dataset_fixture,
         h_feats=32,
     )
